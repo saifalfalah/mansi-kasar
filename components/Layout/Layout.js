cad7d4e710d9bf104eff79f1252c37b0da031173
Nova Gh <nghimire.dev@gmail.com>
2021-06-30 15:15:48 -0700

    Update Layout.js

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,7 +2,7 @@ import Navbar from "../Navbar";
 import Footer from "../Footer";
 const Layout = ({ children }) => {
   return (
-    <div className="px-5 py-5">
+    <div className="p-5 pt-16 pb-2 lg:px-20">
       <Navbar />
       {children}
       <Footer />
