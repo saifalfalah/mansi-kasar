fd36536a4a670637574f6cc0417f8f21164eb47e
Nova Gh <nghimire.dev@gmail.com>
2021-06-30 22:05:36 -0700

    Update IconsComp.js

--- components/homePage/IconsComp.js
+++ components/homePage/IconsComp.js
@@ -1,58 +1,59 @@
 import Image from "next/image";
 const IconsComp = () => {
   return (
-    <div className="w-full h-auto space-y-2 ">
+    <div className="w-full h-auto mt-32 space-y-2">
       <h1 className="font-medium text-center font-poppins lg:text-lg ">
         My Skills
       </h1>
-      <div className="flex flex-wrap justify-center">
+      <div className="flex flex-wrap justify-center ">
         <Image
           src="/assets/skills-icons/ai.png"
           width="59"
-          height="57"
-          alt=""
+          height="59"
+          alt="Adobe Ai Logo"
         />
+
         <Image
           src="/assets/skills-icons/ps.png"
           width="59"
           height="57"
-          alt=""
+          alt="Adobe PS Logo"
         />
         <Image
           src="/assets/skills-icons/figma.png"
           width="59"
           height="57"
-          alt=""
+          alt="Figma Logo"
         />
         <Image
           src="/assets/skills-icons/xd.png"
           width="59"
           height="57"
-          alt=""
+          alt="Adobe XD Logo"
         />
         <Image
           src="/assets/skills-icons/html.png"
           width="59"
           height="57"
-          alt=""
+          alt="HTML Logo"
         />
         <Image
           src="/assets/skills-icons/css.png"
           width="59"
           height="57"
-          alt=""
+          alt="CSS Logo"
         />
         <Image
           src="/assets/skills-icons/angular.png"
           width="59"
           height="57"
-          alt=""
+          alt="Angular Logo"
         />
         <Image
           src="/assets/skills-icons/node.png"
           width="59"
           height="57"
-          alt=""
+          alt="Node Logo"
         />
       </div>
     </div>
