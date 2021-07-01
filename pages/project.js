132fcff0d2d5d99d812c34a233dbb0e5af7fe300
Nova Gh <nghimire.dev@gmail.com>
2021-06-30 17:46:26 -0700

    Create project.js

---
+++ pages/project.js
@@ -0,0 +1,11 @@
+import Meta from "../components/Layout/Meta";
+const project = () => {
+  return (
+    <div className="bg-red-400">
+      <Meta title="Mansi K. | Projects" desc="Mani K UI/UX Portfolio" />
+      <h1 className="text-brand font-poppins">Hello World</h1>
+    </div>
+  );
+};
+
+export default project;
