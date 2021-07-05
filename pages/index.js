3378b9bc4a5439531eace2972a0627ce2b659a14
Nova Gh <nghimire.dev@gmail.com>
2021-07-05 14:29:52 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import IconsComp from "../components/HomeView/SkillsComp";
 import Meta from "../components/Layout/Meta";
 const Home = () => {
   return (
-    <div className="h-screen lg:pl-40 lg:pr-16">
+    <div className=" lg:pl-40 lg:pr-16">
       <Meta title="Mansi K. | Home" desc="Mani K UI/UX Portfolio" />
       <HeroSection />
       <IconsComp />
