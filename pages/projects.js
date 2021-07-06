3371561750e1ba917f8ee00728c7ebd33a97b8eb
Nova Gh <nghimire.dev@gmail.com>
2021-07-05 20:04:41 -0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,20 @@
+import RecentProj from "../components/HomeView/RecentProj";
 import Meta from "../components/Layout/Meta";
+import ProjectList from "../components/ProjectView/AllProj";
+import SideNav from "../components/ProjectView/SideNav";
 const project = () => {
   return (
-    <div className=" lg:pl-36 lg:pr-16">
+    // lg:pl-36 lg:pr-16 1xl:max-w-projectRoute
+    <div className="lg:pl-36 lg:pr-16 lg:mt-32">
       <Meta title="Mansi K. | Projects" desc="Mani K UI/UX Portfolio" />
+      <div className="grid grid-cols-1 md:grid-cols-4 ">
+        <div className="w-48 col-span-1">
+          <SideNav />
+        </div>
+        <div className="col-span-3">
+          <ProjectList />
+        </div>
+      </div>
     </div>
   );
 };
