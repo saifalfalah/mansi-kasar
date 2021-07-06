e2e5b9ed3b7562e4f69ac36fe7ce6e4c6b581b9e
Nova Gh <nghimire.dev@gmail.com>
2021-07-05 20:23:07 -0700

    Create projectCard.js

---
+++ components/Reusable Comp/projectCard.js
@@ -0,0 +1,33 @@
+// card comp without view case study and live preview
+import Image from "next/image";
+import Link from "next/link";
+const projectCard = (props) => {
+  return (
+    <div className="flex flex-col space-y-3 max-w-card font-poppins">
+      <div className="">
+        <Image
+          src={props.imgSrc}
+          width="540"
+          height="489"
+          alt="Picture of dog"
+          className="rounded-3xl"
+        />
+      </div>
+      <h1 className="text-lg font-semibold text-card md:text-2xl ">
+        {props.title}
+      </h1>
+
+      <div className="flex justify-between">
+        <div className="flex items-center space-x-2">
+          <p className="font-medium">
+            <Link href={props.projLink}>
+              <a>View Project</a>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default projectCard;
