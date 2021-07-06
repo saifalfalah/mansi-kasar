2e4c1839c9b809e1a9de7cd5920174343ec8b756
Nova Gh <nghimire.dev@gmail.com>
2021-07-05 20:23:05 -0700

    Update AllProj.js

--- components/ProjectView/AllProj.js
+++ components/ProjectView/AllProj.js
@@ -1,41 +1,36 @@
-import Card from "../Reusable Comp/Card";
+import Card from "../Reusable Comp/projectCard";
 
 const AllProj = () => {
   const projectData = [
     {
-      title: "Test1",
-      caseLink: "https://www.linkedin.com/in/mansi-kasar-363b19184/",
-      liveLink: "https://www.behance.net/mansikasar",
-      imgSrc: "/assets/recent-projects/1.jpg",
+      title: "Coded Websites",
+      projLink: "https://www.linkedin.com/in/mansi-kasar-363b19184/",
+      imgSrc: "/assets/all-projects/1.png",
     },
     {
-      title: "Test2",
-      caseLink: "https://www.linkedin.com/in/mansi-kasar-363b19184/",
-      liveLink: "https://www.behance.net/mansikasar",
-      imgSrc: "/assets/recent-projects/2.jpg",
+      title: "UI/UX Case Studies",
+      projLink: "https://www.linkedin.com/in/mansi-kasar-363b19184/",
+      imgSrc: "/assets/all-projects/2.png",
     },
     {
-      title: "Test3",
-      caseLink: "https://www.linkedin.com/in/mansi-kasar-363b19184/",
-      liveLink: "https://www.behance.net/mansikasar",
-      imgSrc: "/assets/recent-projects/3.jpg",
+      title: "UI/UX Designs",
+      projLink: "https://www.linkedin.com/in/mansi-kasar-363b19184/",
+      imgSrc: "/assets/all-projects/3.png",
     },
     {
-      title: "Test4",
-      caseLink: "https://www.linkedin.com/in/mansi-kasar-363b19184/",
-      liveLink: "https://www.behance.net/mansikasar",
-      imgSrc: "/assets/recent-projects/4.jpg",
+      title: "Logo & Graphic Desgin",
+      projLink: "https://www.linkedin.com/in/mansi-kasar-363b19184/",
+      imgSrc: "/assets/all-projects/4.png",
     },
   ];
   return (
     <div className="">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-7 gap-y-7 md:gap-x-2 lg:gap-x-3 xl:gap-x-1 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-7 gap-y-7 md:gap-x-2 lg:gap-x-6 ">
         {projectData.map((proj, index) => (
           <Card
             key={index}
             title={proj.title}
-            caseLink={proj.caseLink}
-            liveLink={proj.liveLink}
+            projLink={proj.projLink}
             imgSrc={proj.imgSrc}
           />
         ))}
