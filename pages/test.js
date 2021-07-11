5588d6eeb2f68708574c73a0f44941fc25673d65
Nova Gh <nghimire.dev@gmail.com>
2021-07-11 13:53:22 -0700

    test

--- pages/test.js
+++ pages/test.js
@@ -1,15 +1,27 @@
 import Viewer, { Worker } from "@phuocng/react-pdf-viewer";
 import "@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css";
-
+import Image from "next/image";
 const test = () => {
   return (
-    <div className="p-4 lg:px-20 lg:mt-32 lg:mb-12">
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
-        <div style={{ height: "750px" }}>
-          <Viewer fileUrl="/test.pdf" />
-        </div>
-      </Worker>
+    <div className="">
+      <Image
+        src="/test.png"
+        alt="Picture of the author"
+        layout="responsive"
+        width={1266}
+        height={5500}
+        quality={100}
+      />
     </div>
+
+    //  <div className="min-h-screen">
+    //       {/* <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
+    //         <div style={{ height: "750px" }}>
+    //           <Viewer fileUrl="/test.pdf" />
+    //         </div>
+    //       </Worker> */}
+
+    //     </div>
   );
 };
 
