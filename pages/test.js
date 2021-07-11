7c8c4d560981b427301f5c6477e046030c53e877
Nova Gh <nghimire.dev@gmail.com>
2021-07-11 13:13:25 -0700

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -3,7 +3,7 @@ import "@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css";
 
 const test = () => {
   return (
-    <div>
+    <div className="p-4 lg:px-20 lg:mt-32 lg:mb-12">
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
         <div style={{ height: "750px" }}>
           <Viewer fileUrl="/test.pdf" />
