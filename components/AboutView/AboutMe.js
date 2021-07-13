7dc842389d5ff1846a9c14c8c33a61198245c0ca
Nova Gh <nghimire.dev@gmail.com>
2021-07-12 19:22:43 -0700

    Update AboutMe.js

--- components/AboutView/AboutMe.js
+++ components/AboutView/AboutMe.js
@@ -1,10 +1,10 @@
 const AboutMe = () => {
   return (
-    <div className="flex flex-col items-center ">
-      <h1 className="text-3xl font-semibold lg:mb-10 text-heroH lg:text-5xl">
+    <div className="flex flex-col items-center mb-6 lg:mb-11">
+      <h1 className="mb-4 text-3xl font-semibold lg:mb-10 text-heroH lg:text-5xl">
         About <span className="text-brand">Me</span>
       </h1>
-      <p className="text-xl text-center text-heroP lg:max-w-5xl lg:text-2xl">
+      <p className="text-xl text-center text-heroP lg:max-w-5xl lg:text-2xl ">
         I’ve worked with start-ups and small companies, also I’ve created some
         concepts for my personal side projects.
       </p>
