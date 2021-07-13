9ea0f7ab1376611f7f6cdca853cec2e247018525
Nova Gh <nghimire.dev@gmail.com>
2021-07-12 19:11:16 -0700

    Create Roles.js

---
+++ components/AboutView/Roles.js
@@ -0,0 +1,79 @@
+import Image from "next/image";
+// import ui from "..."
+const Roles = () => {
+  const roleItems = [
+    {
+      imgSrc: "/assets/about-icons/ui.svg",
+      imgAlt: "UI Logo",
+      name: "UI/UX Designer",
+    },
+    {
+      imgSrc: "/assets/about-icons/webDev.svg",
+
+      imgAlt: "Computer Icon",
+      name: "Web Developer",
+    },
+    {
+      imgSrc: "/assets/about-icons/webDesigner.svg",
+      imgAlt: "Apps Logo",
+      name: "Web & App Designer",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col justify-between md:max-w-md font-poppins text-heroH">
+      <h1 className="mb-8 text-3xl font-semibold lg:mb-10 lg:text-5xl">
+        Roles <br /> I&apos;ve worked as
+      </h1>
+      <ul className="lg:space-y-12">
+        {roleItems.map((item, index) => (
+          <li key={index} className="flex items-center lg:space-x-8 ">
+            <div className="relative w-16 h-16">
+              <Image
+                src={item.imgSrc}
+                alt={item.imgAlt}
+                layout="fill"
+                objectFit="cover" // contain
+                className=""
+              />
+            </div>
+            <h1 className="text-xl font-normal lg:text-2xl"> {item.name}</h1>
+          </li>
+        ))}
+      </ul>
+      <button
+        href="/projects"
+        className="flex items-center justify-between p-8 text-white bg-black rounded-2xl"
+      >
+        <h1 className="text-4xl font-semibold w-60">
+          Want to see my projects?
+        </h1>
+        <svg
+          c
+          width="48"
+          height="49"
+          viewBox="0 0 48 49"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2 24.009L46 24.009"
+            stroke="white"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M26.75 2L46 24.0093L26.75 46.0186"
+            stroke="white"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Roles;
