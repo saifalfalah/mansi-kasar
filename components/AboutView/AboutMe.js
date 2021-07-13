7f828bf2a493663a7cf2c367f4cc8f892997dd5e
Nova Gh <nghimire.dev@gmail.com>
2021-07-12 19:11:18 -0700

    Create AboutMe.js

---
+++ components/AboutView/AboutMe.js
@@ -0,0 +1,15 @@
+const AboutMe = () => {
+  return (
+    <div className="flex flex-col items-center ">
+      <h1 className="text-3xl font-semibold lg:mb-10 text-heroH lg:text-5xl">
+        About <span className="text-brand">Me</span>
+      </h1>
+      <p className="text-xl text-center text-heroP lg:max-w-5xl lg:text-2xl">
+        I’ve worked with start-ups and small companies, also I’ve created some
+        concepts for my personal side projects.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
