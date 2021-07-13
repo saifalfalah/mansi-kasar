7617bb7c129c391484c044be6ec90711eb1f3b3b
Nova Gh <nghimire.dev@gmail.com>
2021-07-12 19:22:35 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ import Meta from "../components/Layout/Meta";
 
 const about = () => {
   return (
-    <div className="p-8 ">
+    <div className="p-8 lg:p-0 ">
       <Meta title="Mansi K. | About" desc="Mani K UI/UX Portfolio" />
       <AboutMe />
       <div className="grid gap-20 mx-auto lg:gap-40 grid-col-1 lg:grid-cols-2 lg:max-w-6xl md:max-w-5xl lg:mb-10">
