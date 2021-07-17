d238ebb24c16f5d082bd01926a7b082706d18c80
Nova Gh <nghimire.dev@gmail.com>
2021-07-16 18:08:44 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,39 @@ module.exports = {
     extend: {
       colors: {
         brand: "#7140FF",
+        brandLight: "#D7A6FF",
         brandOutline: "#9B51E0",
         navItems: "#151517",
         heroP: "#828282",
         heroH: "#2F2F2F",
         card: "#1C1C1C",
         bar: "#E5E5E5",
+        divider: "#717171",
+        exp: "#121212",
       },
+
       fontSize: {
         xss: ["8px", "12px"],
+      },
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient(ellipse at center, var(--tw-gradient-stops))",
+        "gradient-radial-at-t":
+          "radial-gradient(ellipse at top, var(--tw-gradient-stops))",
+        "gradient-radial-at-b":
+          "radial-gradient(ellipse at bottom, var(--tw-gradient-stops))",
+        "gradient-radial-at-l":
+          "radial-gradient(ellipse at left, var(--tw-gradient-stops))",
+        "gradient-radial-at-r":
+          "radial-gradient(ellipse at right, var(--tw-gradient-stops))",
+        "gradient-radial-at-tl":
+          "radial-gradient(ellipse at top left, var(--tw-gradient-stops))",
+        "gradient-radial-at-tr":
+          "radial-gradient(ellipse at top right, var(--tw-gradient-stops))",
+        "gradient-radial-at-bl":
+          "radial-gradient(ellipse at bottom left, var(--tw-gradient-stops))",
+        "gradient-radial-at-br":
+          "radial-gradient(ellipse at bottom right, var(--tw-gradient-stops))",
       },
       spacing: {
         97: "33.75rem",
