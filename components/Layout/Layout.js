27b2c986a1a33cc2c61f79bd1654fcad69b30338
Nova Gh <nghimire.dev@gmail.com>
2021-07-16 21:31:13 -0700

    Update Layout.js
    added scrolltotopbutton

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import ScrollTopBtn from "../Reusable Comp/ScrollTopBtn";
+
 const Layout = ({ children }) => {
   return (
     // p-5 pt-16 pb-2 lg:px-20"
-    <div className="min-h-screen">
+    <div className="min-h-screen ">
       <Navbar />
       {children}
+      <ScrollTopBtn />
       <Footer />
     </div>
   );
