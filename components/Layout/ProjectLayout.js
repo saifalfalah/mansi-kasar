bad8b14614f7cb1d6784f1997f7d0dd3df2c2e4a
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 10:49:40 -0700

    Create ProjectLayout.js

---
+++ components/Layout/ProjectLayout.js
@@ -0,0 +1,19 @@
+import ProjectList from "../ProjectView/AllProj";
+import SideNav from "../ProjectView/SideNav";
+import Meta from "./Meta";
+
+const ProjectLayout = (props) => {
+  return (
+    <div className="p-4 lg:px-20 lg:mt-32 lg:mb-12">
+      <Meta title={props.title} />
+      <div className="grid grid-cols-1 md:grid-cols-4 ">
+        <div className="w-48 col-span-1 mb-10 lg:mb-0">
+          <SideNav />
+        </div>
+        <div className="col-span-3">{props.children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectLayout;
