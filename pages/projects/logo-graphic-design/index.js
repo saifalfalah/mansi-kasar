ebf187debd1c37f01a44c89085891e24c00f0734
Nova Gh <nghimire.dev@gmail.com>
2021-07-20 00:03:18 -0700

    Update index.js

--- pages/projects/logo-graphic-design/index.js
+++ pages/projects/logo-graphic-design/index.js
@@ -3,7 +3,9 @@ import ProjectListLayout from "../../../components/Layout/ProjectListLayout";
 const logoGraphicDesign = () => {
   return (
     <ProjectListLayout title="M K.|Logo & Graphic Design">
-      Test
+      <div className="flex flex-col items-center space-y-20 lg:items-stretch lg:flex-col lg:space-y-28">
+        Comming Soon
+      </div>
     </ProjectListLayout>
   );
 };
