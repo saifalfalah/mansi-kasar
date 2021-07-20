6735542e6e01019cb5ffed2f3c55fef09672d6c6
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 20:10:04 -0700

    Update uiuxDesign.data.js

--- data/uiuxDesign.data.js
+++ data/uiuxDesign.data.js
@@ -1,26 +1,33 @@
 export const uiuxDesignList = [
   {
     id: 0,
-    title: "not comp",
+    title: "UI/UX Design 1",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     contentSrc: "/assets/all-projects/uixCaseStudy/eachDesign/1.png",
-    thumbnailSrc: "/assets/all-projects/uixDesign/1.png",
+    thumbnailSrc: "/assets/all-projects/uiuxDesign/1.png",
+    textCont: "",
+    imgCont: "",
   },
   {
     id: 1,
-    title: "Tassel 2",
+    title: "UI/UX Design 2",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     contentSrc: "/assets/all-projects/uixDesign/eachDesign/2.png",
-    thumbnailSrc: "/assets/all-projects/uixDesign/2.png",
+    thumbnailSrc: "/assets/all-projects/uiuxDesign/2.png",
+
+    textCont: "order-2",
+    imgCont: "order-1",
   },
   {
     id: 2,
-    title: "Tassel 3",
+    title: "UI/UX Design 3",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     contentSrc: "/assets/all-projects/uixDesign/eachDesign/3.png",
-    thumbnailSrc: "/assets/all-projects/uixDesign/3.png",
+    thumbnailSrc: "/assets/all-projects/uiuxDesign/3.png",
+    textCont: "",
+    imgCont: "",
   },
 ];
