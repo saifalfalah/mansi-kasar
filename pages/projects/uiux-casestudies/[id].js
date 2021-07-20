b1bd360e21e002946955144084b5fbff37f60667
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 23:41:04 -0700

    Update [id].js

--- pages/projects/uiux-casestudies/[id].js
+++ pages/projects/uiux-casestudies/[id].js
@@ -1,38 +1,38 @@
 // import { uiuxCaseList } from "../../../components/data/uiuxCasestudies.data";
-import { uiuxCaseList } from "/data/uiuxCasestudies.data";
+import { uiuxDesignList } from "../../../data/uiuxDesign.data";
 import ProjectListLayout from "../../../components/Layout/ProjectListLayout";
 import Image from "next/image";
 export const getStaticProps = async ({ params }) => {
   //put the uiuxcaselist as caseLists and filter out the id and makse sure its the parsm.id
-  const caseLists = uiuxCaseList.filter((p) => p.id.toString() === params.id);
+  const caseLists = uiuxDesignList.filter((p) => p.id.toString() === params.id);
 
   return {
     props: {
       //return the id which is the first index(0)
-      singleCase: caseLists[0],
+      singleDesign: caseLists[0],
     },
     revalidate: 10,
   };
 };
 export const getStaticPaths = async () => {
-  const paths = uiuxCaseList.map((singleCase) => ({
-    params: { id: singleCase.id.toString() },
+  const paths = uiuxDesignList.map((singleDesign) => ({
+    params: { id: singleDesign.id.toString() },
   }));
   return { paths, fallback: false };
 };
 
-const CaseStudy = ({ singleCase }) => {
+const CaseStudy = ({ singleDesign }) => {
   return (
     <ProjectListLayout>
       <h1 className="text-xl font-medium text-exp lg:text-3xl">
-        {singleCase.title}
+        {singleDesign.title}
       </h1>
       <Image
-        src={singleCase.contentSrc}
+        src={singleDesign.contentSrc}
         alt="Picture of the author"
         layout="responsive"
-        width={1266}
-        height={5500}
+        width={200}
+        height={200}
         quality={100}
       />
     </ProjectListLayout>
