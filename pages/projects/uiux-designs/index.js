5482dbc7bea01e93ab0769a2f3e8b69882b2b691
Nova Gh <nghimire.dev@gmail.com>
2021-07-20 13:25:16 -0700

    Update index.js

--- pages/projects/uiux-designs/index.js
+++ pages/projects/uiux-designs/index.js
@@ -1,12 +1,12 @@
 import ProjectListLayout from "../../../components/Layout/ProjectListLayout";
 import DesignCard from "../../../components/ProjectView/UiUxDesign/DesignCard";
 import { uiuxDesignList } from "/data/uiuxDesign.data";
-const uiuxDesigns = ({ data }) => {
+const uiuxDesigns = ({ dataDesign }) => {
   // console.log(data);
   return (
     <ProjectListLayout title="UI/UX Case Studies">
       <div className="flex flex-col items-center space-y-20 lg:items-stretch lg:flex-col lg:space-y-28">
-        {data.map((items, index) => (
+        {dataDesign.map((items, index) => (
           <DesignCard key={index} {...items} />
         ))}
       </div>
@@ -18,7 +18,7 @@ export default uiuxDesigns;
 export const getStaticProps = async (context) => {
   return {
     props: {
-      data: uiuxDesignList,
+      dataDesign: uiuxDesignList,
     },
     revalidate: 10,
   };
