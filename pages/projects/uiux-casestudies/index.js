7e3a99d68e79e416797b1028a98c4080112924c0
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 19:19:21 -0700

    Update index.js

--- pages/projects/uiux-casestudies/index.js
+++ pages/projects/uiux-casestudies/index.js
@@ -2,7 +2,7 @@ import ProjectListLayout from "../../../components/Layout/ProjectListLayout";
 import CaseStudyCard from "../../../components/ProjectView/UiUxCaseStudy/CaseStudyCard";
 import { uiuxCaseList } from "/data/uiuxCasestudies.data";
 const uiuxCaseStudies = ({ data }) => {
-  console.log(data);
+  // console.log(data);
   return (
     <ProjectListLayout title="UI/UX Case Studies">
       <div className="flex flex-col items-center space-y-20 lg:items-stretch lg:flex-col lg:space-y-28">
