fb02bf633565a0cb4952fe4ecb870d34a847c708
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 20:13:03 -0700

    Update uiuxDesign.data.js

--- data/uiuxDesign.data.js
+++ data/uiuxDesign.data.js
@@ -6,8 +6,8 @@ export const uiuxDesignList = [
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     contentSrc: "/assets/all-projects/uixCaseStudy/eachDesign/1.png",
     thumbnailSrc: "/assets/all-projects/uiuxDesign/1.png",
-    textCont: "",
-    imgCont: "",
+    textCont: "order-2 lg:order-none",
+    imgCont: "order-1 lg:order-none",
   },
   {
     id: 1,
@@ -27,7 +27,7 @@ export const uiuxDesignList = [
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     contentSrc: "/assets/all-projects/uixDesign/eachDesign/3.png",
     thumbnailSrc: "/assets/all-projects/uiuxDesign/3.png",
-    textCont: "",
-    imgCont: "",
+    textCont: "order-2 lg:order-none",
+    imgCont: "order-1 lg:order-none",
   },
 ];
