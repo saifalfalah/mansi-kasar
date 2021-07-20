aa4f01e4af07c2bd4912a2e1052b1a07481de9dc
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 18:45:50 -0700

    Update index.js

--- pages/projects/coded-websites/index.js
+++ pages/projects/coded-websites/index.js
@@ -1,8 +1,17 @@
 import ProjectListLayout from "../../../components/Layout/ProjectListLayout";
+import WebsitesCard from "../../../components/ProjectView/CodedWebsites/WebsitesCard";
 import { codedWebsiteList } from "/data/codedWebsite.data";
 const codedWebsites = ({ data }) => {
   console.log(data);
-  return <ProjectListLayout title="M K.|Coded Websites"></ProjectListLayout>;
+  return (
+    <ProjectListLayout title="M K.|Coded Websites">
+      <div className="flex flex-col items-center space-y-20 lg:items-stretch lg:flex-col lg:space-y-28">
+        {data.map((items, index) => (
+          <WebsitesCard key={index} {...items} />
+        ))}
+      </div>
+    </ProjectListLayout>
+  );
 };
 
 export default codedWebsites;
