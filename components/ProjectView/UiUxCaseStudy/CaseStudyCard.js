4a50329d6e37e09b8bb3c6f7a870636ce48b8cd4
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 17:00:54 -0700

    Update CaseStudyCard.js

--- components/ProjectView/UiUxCaseStudy/CaseStudyCard.js
+++ components/ProjectView/UiUxCaseStudy/CaseStudyCard.js
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
-const CaseStudyCard = () => {
+const CaseStudyCard = (props) => {
   return (
     <div className="flex flex-col justify-between font-poppins lg:flex-row ">
       <div className="order-2 max-w-xs space-y-3 lg:order-none ">
-        <h1 className="text-xl font-medium text-exp lg:text-3xl">Tassel</h1>
-        <p className=" lg:text-lg text-expSec">
-          UI & UX design for a fashion marketplace.Personal conceptual side
-          project.
-        </p>
-        <Link href="/" passHref>
+        <h1 className="text-xl font-medium text-exp lg:text-3xl">
+          {props.title}
+        </h1>
+        <p className=" lg:text-lg text-expSec">{props.summary}</p>
+        <Link
+          href={`/projects/uiux-casestudies/[id]`}
+          as={`/projects/uiux-casestudies/${props.id}`}
+          passHref
+        >
           <button className="px-4 py-3 text-white bg-brand rounded-3xl ">
             Read Case Study
           </button>
@@ -17,10 +20,10 @@ const CaseStudyCard = () => {
       </div>
       <div className="order-1 max-w-md max-h-60 lg:order-none">
         <Image
-          src="/assets/all-projects/uixCaseStudy/1.png"
+          src={props.imgSrc}
           width="464"
           height="236"
-          alt="Picture of dog"
+          alt={props.title}
           className="rounded-3xl"
         />
       </div>
