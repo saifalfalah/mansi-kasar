c9c731416fc900f65e86e35aeb84450745dff500
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 23:41:01 -0700

    Update uiuxDesign.data.js

--- data/uiuxDesign.data.js
+++ data/uiuxDesign.data.js
@@ -4,7 +4,7 @@ export const uiuxDesignList = [
     title: "UI/UX Design 1",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
-    contentSrc: "/assets/all-projects/uixCaseStudy/eachDesign/1.png",
+    contentSrc: "/assets/all-projects/uiuxDesign/eachDesign/1.png",
     thumbnailSrc: "/assets/all-projects/uiuxDesign/1.png",
     textCont: "order-2 lg:order-none",
     imgCont: "order-1 lg:order-none",
@@ -14,7 +14,7 @@ export const uiuxDesignList = [
     title: "UI/UX Design 2",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
-    contentSrc: "/assets/all-projects/uixDesign/eachDesign/2.png",
+    contentSrc: "/assets/all-projects/uiuxDesign/eachDesign/2.png",
     thumbnailSrc: "/assets/all-projects/uiuxDesign/2.png",
 
     textCont: "order-2",
@@ -25,7 +25,7 @@ export const uiuxDesignList = [
     title: "UI/UX Design 3",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
-    contentSrc: "/assets/all-projects/uixDesign/eachDesign/3.png",
+    contentSrc: "/assets/all-projects/uiuxDesign/eachDesign/3.png",
     thumbnailSrc: "/assets/all-projects/uiuxDesign/3.png",
     textCont: "order-2 lg:order-none",
     imgCont: "order-1 lg:order-none",
