52971625debdb338e0032abeebf4980ed4dc4a1e
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 17:00:59 -0700

    Update uiuxCasestudies.data.js

--- data/uiuxCasestudies.data.js
+++ data/uiuxCasestudies.data.js
@@ -1,7 +1,7 @@
 export const uiuxCaseList = [
   {
     id: 0,
-    title: "Tassel 1",
+    title: "not comp",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     imgSrc: "/assets/all-projects/uixCaseStudy/1.png",
