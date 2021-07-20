6008c1aedbf646a5dc3d27a5c252d69a2f2ab46f
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 23:41:07 -0700

    Update [id].js

---
+++ pages/projects/uiux-designs/[id].js
@@ -0,0 +1,45 @@
+// import { uiuxCaseList } from "../../../components/data/uiuxCasestudies.data";
+import { uiuxDesignList } from "/data/uiuxCasestudies.data";
+import ProjectListLayout from "../../../components/Layout/ProjectListLayout";
+import Image from "next/image";
+export const getStaticProps = async ({ params }) => {
+  //put the uiuxcaselist as caseLists and filter out the id and makse sure its the parsm.id
+  const designLists = uiuxDesignList.filter(
+    (p) => p.id.toString() === params.id
+  );
+
+  return {
+    props: {
+      //return the id which is the first index(0)
+      singleDesign: designLists[0],
+    },
+    revalidate: 10,
+  };
+};
+export const getStaticPaths = async () => {
+  const paths = uiuxCaseList.map((singleDesign) => ({
+    params: { id: singleDesign.id.toString() },
+  }));
+  return { paths, fallback: false };
+};
+
+const Design = ({ singleDesign }) => {
+  console.log(singleDesign.title);
+  return (
+    <ProjectListLayout>
+      <h1 className="text-xl font-medium text-exp lg:text-3xl">
+        {singleDesign.title}
+      </h1>
+      <Image
+        src={singleDesign.contentSrc}
+        alt="Picture of the author"
+        layout="responsive"
+        width={1266}
+        height={5500}
+        quality={100}
+      />
+    </ProjectListLayout>
+  );
+};
+
+export default Design;
