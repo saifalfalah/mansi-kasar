5fc7b476512eb215d1d6eca2aba1bf86bd013148
Nova Gh <nghimire.dev@gmail.com>
2021-07-19 18:28:45 -0700

    Update codedWebsite.data.js

--- data/codedWebsite.data.js
+++ data/codedWebsite.data.js
@@ -1,23 +1,26 @@
 export const codedWebsiteList = [
   {
     id: 0,
-    title: "Best UI/UX tools for 2021",
-    summary:
-      "From figma to XD, here i am gonna talk about the best tools for UI/UX design in 2020.",
-    blogLink: "https://google.com",
+    title: "Credit Card Validator",
+    summary: "Validates credit card numbers using Lunh’s Algorithm test",
+    viewLink: "https://google.com",
+    gitLink: "https://github.com",
+    thumbnailImg: "",
   },
   {
     id: 1,
-    title: "Best UI/UX tools for 2021",
-    summary:
-      "From figma to XD, here i am gonna talk about the best tools for UI/UX design in 2020.",
-    blogLink: "https://google.com",
+    title: "Website 2",
+    summary: "Validates credit card numbers using Lunh’s Algorithm test",
+    viewLink: "https://google.com",
+    gitLink: "https://github.com",
+    thumbnailImg: "",
   },
   {
     id: 2,
-    title: "How to make Glass effect in figma",
-    summary:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt sed consequat, lacus, in metus, porttitor justo.",
-    blogLink: "https://google.com",
+    title: "Website 3",
+    summary: "Validates credit card numbers using Lunh’s Algorithm test",
+    viewLink: "https://google.com",
+    gitLink: "https://github.com",
+    thumbnailImg: "",
   },
 ];
