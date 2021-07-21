bc75bda8a1958cc45d3874c860dd47d1435322d1
Nova Gh <nghimire.dev@gmail.com>
2021-07-21 16:57:36 -0700

    Update Roles.js

--- components/AboutView/Roles.js
+++ components/AboutView/Roles.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+
 // import ui from "..."
 const Roles = () => {
   const roleItems = [
@@ -41,36 +43,35 @@ const Roles = () => {
           </li>
         ))}
       </ul>
-      <button
-        href="/projects"
-        className="flex items-center justify-between p-8 mt-4 text-white bg-black lg:mt-0 rounded-2xl"
-      >
-        <h1 className="text-4xl font-semibold w-60">
-          Want to see my projects?
-        </h1>
-        <svg
-          width="48"
-          height="49"
-          viewBox="0 0 48 49"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M2 24.009L46 24.009"
-            stroke="white"
-            strokeWidth="4"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M26.75 2L46 24.0093L26.75 46.0186"
-            stroke="white"
-            strokeWidth="4"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </button>
+      <Link href="/projects" passHref>
+        <button className="flex items-center justify-between p-8 mt-4 text-white bg-black lg:mt-0 rounded-2xl">
+          <h1 className="text-4xl font-semibold w-60">
+            Want to see my projects?
+          </h1>
+          <svg
+            width="48"
+            height="49"
+            viewBox="0 0 48 49"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 24.009L46 24.009"
+              stroke="white"
+              strokeWidth="4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M26.75 2L46 24.0093L26.75 46.0186"
+              stroke="white"
+              strokeWidth="4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </Link>
     </div>
   );
 };
