59c098750bb489bd489fc44ae1f167688eb8c81d
Nova Gh <nghimire.dev@gmail.com>
2021-07-22 23:23:07 -0700

    Update index.js

--- pages/projects/logo-graphic-design/index.js
+++ pages/projects/logo-graphic-design/index.js
@@ -1,13 +1,151 @@
+import Image from "next/image";
 import ProjectListLayout from "../../../components/Layout/ProjectListLayout";
 
 const logoGraphicDesign = () => {
   return (
     <ProjectListLayout title="M K.|Logo & Graphic Design">
-      <div className="flex flex-col items-center space-y-20 lg:items-stretch lg:flex-col lg:space-y-28">
-        Comming Soon
+      <div className="flex flex-wrap justify-center max-w-5xl gap-6 ">
+        <div className="max-w-xs max-h-60">
+          <Image
+            src="/assets/all-projects/LogoGraphicDesign/2.png"
+            width="280"
+            height="191"
+            alt="test"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="max-w-xs max-h-60">
+          <Image
+            src="/assets/all-projects/LogoGraphicDesign/1.png"
+            width="280"
+            height="191"
+            alt="test"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="max-w-xs max-h-60">
+          <Image
+            src="/assets/all-projects/LogoGraphicDesign/1.png"
+            width="280"
+            height="191"
+            alt="test"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="max-w-xs max-h-60">
+          <Image
+            src="/assets/all-projects/LogoGraphicDesign/1.png"
+            width="280"
+            height="191"
+            alt="test"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="max-w-xs max-h-60">
+          <Image
+            src="/assets/all-projects/LogoGraphicDesign/1.png"
+            width="280"
+            height="191"
+            alt="test"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="max-w-xs max-h-60">
+          <Image
+            src="/assets/all-projects/LogoGraphicDesign/1.png"
+            width="280"
+            height="191"
+            alt="test"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="max-w-xs max-h-60">
+          <Image
+            src="/assets/all-projects/LogoGraphicDesign/1.png"
+            width="280"
+            height="191"
+            alt="test"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="max-w-xs max-h-60">
+          <Image
+            src="/assets/all-projects/LogoGraphicDesign/1.png"
+            width="280"
+            height="191"
+            alt="test"
+            className="rounded-3xl"
+          />
+        </div>
       </div>
     </ProjectListLayout>
   );
 };
 
 export default logoGraphicDesign;
+
+{
+  /*
+  
+   <div className="relative h-72 w-72">
+    <Image
+      src="/assets/all-projects/LogoGraphicDesign/1.png"
+      layout="fill"
+      objectFit="contain"
+      alt="test"
+      className="rounded-3xl"
+    />
+  </div>
+
+
+
+  <div className="flex flex-col flex-wrap justify-between lg:flex-row ">
+<div className="grid grid-cols-12 grid-rows-6 gap-10">
+  <div className="col-span-4 row-span-5">
+    <Image
+      src="/assets/all-projects/LogoGraphicDesign/1.png"
+      width="280 "
+      height="280"
+      alt="test"
+      className="rounded-3xl"
+    />
+  </div>
+  <div className=" max-w-max">
+    <Image
+      src="/assets/all-projects/LogoGraphicDesign/1.png"
+      width="156 "
+      height="156"
+      alt="test"
+      className="rounded-3xl"
+    />
+  </div>
+  <div className=" max-w-max">
+    <Image
+      src="/assets/all-projects/LogoGraphicDesign/1.png"
+      width="344 "
+      height="156"
+      alt="test"
+      className="rounded-3xl"
+    />
+  </div>
+  <div className="max-w-max">
+    <Image
+      src="/assets/all-projects/LogoGraphicDesign/1.png"
+      width="280 "
+      height="191"
+      alt="test"
+      className="rounded-3xl"
+    />
+  </div>
+  <div className="max-w-max">
+    <Image
+      src="/assets/all-projects/LogoGraphicDesign/1.png"
+      width="280 "
+      height="314"
+      alt="test"
+      className="rounded-3xl"
+    />
+  </div>
+</div>
+</div> */
+}
