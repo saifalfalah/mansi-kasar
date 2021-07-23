f9460a5d205d785aec8a27f1903e6445d5732714
Nova Gh <nghimire.dev@gmail.com>
2021-07-22 23:23:01 -0700

    Update ProjectLayout.js

--- components/Layout/ProjectLayout.js
+++ components/Layout/ProjectLayout.js
@@ -3,7 +3,7 @@ import Meta from "./Meta";
 
 const ProjectLayout = (props) => {
   return (
-    <div className="p-4 lg:px-20 lg:mt-32 lg:mb-12">
+    <div className="p-4 xl:pl-36 xl:pr-16 lg:mt-32 lg:mb-12">
       <Meta title={props.title} />
       <div className="grid grid-cols-1 md:grid-cols-4 ">
         <div className="w-48 col-span-1 mb-10 lg:mb-0">
