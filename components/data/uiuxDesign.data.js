164b373dfd01eb96f7af116a1f2013e4ae2ef790
Nova Gh <nghimire.dev@gmail.com>
2021-07-26 16:40:21 -0700

    Update uiuxDesign.data.js

--- components/data/uiuxDesign.data.js
+++ components/data/uiuxDesign.data.js
@@ -1,7 +1,7 @@
 export const uiuxDesignList = [
   {
     id: 0,
-    title: "UI/UX Design 1",
+    title: "Art Venture",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     contentSrc: "/assets/all-projects/uiuxDesign/eachDesign/1.png",
@@ -12,7 +12,7 @@ export const uiuxDesignList = [
   },
   {
     id: 1,
-    title: "UI/UX Design 2",
+    title: "College Fynd",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     contentSrc: "/assets/all-projects/uiuxDesign/eachDesign/2.png",
@@ -23,11 +23,33 @@ export const uiuxDesignList = [
   },
   {
     id: 2,
-    title: "UI/UX Design 3",
+    title: "Binge",
     summary:
       "UI & UX design for a fashion marketplace. Personal conceptual side project.",
     contentSrc: "/assets/all-projects/uiuxDesign/eachDesign/3.png",
     thumbnailSrc: "/assets/all-projects/uiuxDesign/3.png",
+    textCont: "order-2 lg:order-none",
+    imgCont: "order-1 lg:order-none",
+    videoUrl: "https://www.youtube.com/embed/DEiVBwIavJo",
+  },
+  {
+    id: 3,
+    title: "Music App",
+    summary:
+      "UI & UX design for a fashion marketplace. Personal conceptual side project.",
+    contentSrc: "/assets/all-projects/uiuxDesign/eachDesign/4.png",
+    thumbnailSrc: "/assets/all-projects/uiuxDesign/4.png",
+    textCont: "order-2",
+    imgCont: "order-1",
+    videoUrl: "https://www.youtube.com/embed/DEiVBwIavJo",
+  },
+  {
+    id: 4,
+    title: "Coffee App",
+    summary:
+      "UI & UX design for a fashion marketplace. Personal conceptual side project.",
+    contentSrc: "/assets/all-projects/uiuxDesign/eachDesign/5.png",
+    thumbnailSrc: "/assets/all-projects/uiuxDesign/5.png",
     textCont: "order-2 lg:order-none",
     imgCont: "order-1 lg:order-none",
     videoUrl: "https://www.youtube.com/embed/DEiVBwIavJo",
